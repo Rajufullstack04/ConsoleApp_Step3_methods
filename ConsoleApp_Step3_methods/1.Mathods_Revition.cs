3109bf9a24194a228d9b1a0a4473bff24394f80f
Rajufullstack04 <rajufullstack777@gmail.com>
2025-11-27 08:17:09 +0530

    Add project files.

---
+++ ConsoleApp_Step3_methods/1.Mathods_Revition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathContainer
+{
+     class Program
+    {
+
+        // Mathod that process an integer array and rturns a string array
+        static string[] ProcessArray(int[] numbers)
+        {
+            //string[] result = new string[3];
+            //{"raju","hguh","hgi" }
+
+
+            string[] result = new string[numbers.Length];
+
+            for(int i = 0; i < numbers.Length; i++ )
+            {
+                if (numbers[i] %2 == 0 )
+                {
+                    result[i] = $"{numbers[i]} is Even Number";
+                }
+                else
+                {
+                    result[i] = $"{numbers[i]} is Add Number";
+                }
+            }
+            // return string array
+            return result;
+        } 
+        static void Main()
+        {
+            int[] numbers = { 15, 14 };
+            string[] output = ProcessArray(numbers);
+
+            foreach (string result in output )
+            {
+                Console.WriteLine(result);
+            }
+
+
+
+
+            //Console.WriteLine(output);
+        }
+    }
+}
---
+++ ConsoleApp_Step3_methods/2.Mathods_Advanced.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+
+
+namespace Mathod_Advanced_container
+{
+    class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Subject { get; set; }
+
+    }
+    class  StudentOperation
+    {
+
+        /// <summary>
+        ///  Returns a single student as a class model.
+        /// </summary>
+        /// <returns>Studnet Model</returns>
+        public static Student GetStudent()
+        {
+            //Student student = new Student();
+            //Student.ID = 1;
+            //Student.Name = "Raju";
+            //Student.Age = 25;
+            //Student.Subject = "C#.Net";
+            //return Student;
+
+            return new Student()
+            {
+                ID = 1,
+                Name = "Raju",
+                Age = 25,
+                Subject = "C#.Net"
+
+            };
+
+        }
+        /// <summary>
+        /// Returns a list of students as class models.
+        /// </summary>
+        /// <returns>Students array</returns>
+        public static Student[] GetStudents()
+        {
+            return new Student[]
+            {
+                new Student {ID=2 ,Name = "Venky",Age = 27,Subject ="Javascript"},
+                new Student {ID=3 ,Name = "Adarsh",Age = 23,Subject ="Angular"},
+                new Student {ID=4 ,Name = "Sathiesh",Age = 21,Subject ="HTML&CSS"}
+
+            };
+
+
+        }
+        /// <summary>
+        ///  Takes a student as input and returns the same student with updated details.
+        /// </summary>
+        /// <param name="student">Type is Studnet</param>
+        /// <returns>Student</returns>
+        public static Student UpdateStudent(Student student)
+        {
+            student.Subject = student.Subject + " - Advanced Leval";
+            return student;
+          
+        }
+        /// <summary>
+        ///  Takes a list of students as input and returns the updated list of students.
+        /// </summary>
+        /// <param name="students">stuenetdnts type</param>
+        /// <returns>stuendts</returns>
+
+        public static Student[] UpdateStudents(Student[] students)
+        {
+            foreach (Student student in students)
+            {
+                student.Subject = student.Subject + " - Advanced";
+            }
+            return students;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            // Method 1: Return a single class model
+            var singleStudent = StudentOperation.GetStudent();
+            Console.WriteLine($"Single Student: {singleStudent.Name}, {singleStudent.Age}, {singleStudent.Subject}");
+
+
+            // Method 2: Return a list of class models
+            Student[] studnetsList = StudentOperation.GetStudents();
+            // var studnetsList = StudentOperations.GetStudents();
+            Console.WriteLine("List of Students:");
+            foreach (var student in studnetsList)
+            {
+                Console.WriteLine($"Single Student: {student.Name}, {student.Age}, {student.Subject}");
+            }
+
+
+            // Method 3: Take a class model as input and return an updated class model
+            Student updatedStudent = StudentOperation.UpdateStudent(singleStudent);
+
+            // Method 4: Take a list of class models as input and return an updated list of class models
+            var updatedStudetns = StudentOperation.UpdateStudents(studnetsList);
+            Console.WriteLine("List of Students:");
+            foreach (var student in updatedStudetns)
+            {
+                Console.WriteLine($"Single Student: {student.Name}, {student.Age}, {student.Subject}");
+            }
+
+
+        }
+    }
+
+
+
+}
+
+
+
+//GetStudent . name , age , subject
+//GetStudentsList -- each student name , age , subject
+
+
+//Update subject  - which student you have to update?????(input parameter)
+//update subject for the studnents.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
